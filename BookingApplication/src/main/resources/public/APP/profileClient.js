a5bcb7fc788561a9583c703771c852f02ebf5a46
MitarBrankovic <brankovic.ra198.2018@uns.ac.rs>
2022-01-04 20:31:50 +0100

    fix 3.2: fixed validation and css

--- BookingApplication/src/main/resources/public/APP/profileClient.js
+++ BookingApplication/src/main/resources/public/APP/profileClient.js
@@ -2,6 +2,7 @@ Vue.component("ProfileClient", {
     data: function() {
         return {
             activeUser:"",
+            helperUser:"",
             editClick:false,
             requestDelete:false,
             sendCheck:false,
@@ -10,121 +11,113 @@ Vue.component("ProfileClient", {
     },
     template : ` 
     <div>
-        <div v-if="editClick==true" class="forma container">
-            <form id="editForm" method ="PUT" @submit.prevent = "submitForm">
-                <h1>Edit profile</h1>
-                <hr>
-            
+        <h1 style="height: 10%;">Profile info</h1>
+        <div>
+            <form id="editForm" style="height: 100%; width: 100%;" method ="PUT" @submit.prevent = "submitForm">
 			    <label class="col-sm-2 col-form-label" for="name"><b>Name</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.name" required>
+			    <input id="name" pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-2 col-form-control" type="text" v-model="activeUser.name" disabled required>
 				<br>
                 
 				<label class="col-sm-2 col-form-label" for="surname"><b>Surname</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.surname" required>
+			    <input id="surname" pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-2 col-form-control" type="text" v-model="activeUser.surname" disabled required>
 				<br>
                 
                 
 				<label class="col-sm-2 col-form-label" for="email"><b>Email</b></label>
-			    <input class="col-sm-2 col-form-control" type="email" v-model="activeUser.email" readonly>
+			    <input id="email" class="col-sm-2 col-form-control" type="email" v-model="activeUser.email" disabled readonly>
 				<br>
                 
                 <label class="col-sm-2 col-form-label" for="password"><b>Password</b></label>
-			    <input id="password" class="col-sm-2 col-form-control" type="password" v-model="activeUser.password" @change='check_pass()' required>
+			    <input id="password" class="col-sm-2 col-form-control" type="password" v-model="activeUser.password" @change='check_pass()' disabled required>
 				<br>
                 
 			    <label class="col-sm-2 col-form-label" for="password-repeat"><b>Repeat password</b></label>
-			    <input id="confirmPassword" class="col-sm-2 col-form-control" type="password" @change='check_pass()' required>
-			    <hr>
+			    <input id="confirmPassword" class="col-sm-2 col-form-control" type="password" @change='check_pass()' disabled required>
+			    <br>
                 
 				<label class="col-sm-2 col-form-label" for="address"><b>Address</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.address" required>
+			    <input id="address" class="col-sm-2 col-form-control" type="text" v-model="activeUser.address" disabled required>
 				<br>
                 
                 <label class="col-sm-2 col-form-label" for="city"><b>City</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.city" required>
+			    <input id="city" pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-2 col-form-control" type="text" v-model="activeUser.city" disabled required>
 				<br>
 
                 <label class="col-sm-2 col-form-label" for="country"><b>Country</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.country" required>
+			    <input id="country" pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-2 col-form-control" type="text" v-model="activeUser.country" disabled required>
 				<br>
 
                 <label class="col-sm-2 col-form-label" for="phoneNumber"><b>PhoneNumber</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.phoneNumber" required>
-
-                
-                <hr>
-                <button id="submit" type="submit" class="button" disabled>Save</button>
-                <button type="button" class="button" v-on:click="editClick=false">Cancel</button>
+			    <input id="phoneNumber" pattern="[0-9]+" title="Enter numbers only." class="col-sm-2 col-form-control" type="text" v-model="activeUser.phoneNumber" disabled required>
+                <br>
+                <button v-if="editClick==false" type="button" class="button" v-on:click="isEdit()">Edit</button>
+                <button v-if="editClick==true" id="submit" type="submit" class="btn btn-success" disabled>Save</button>
+                <button v-if="editClick==true" type="button" class="btn btn-secondary" v-on:click="cancelEdit()">Cancel</button>
             </form>
         </div>
-        <div v-else class="forma container">
-            <h1>Profile info</h1>
-            <hr>
-        
-            <label class="col-sm-2 col-form-label" for="name"><b>Name</b></label>
-            <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.name"  readonly>
-            <br>
-            
-            <label class="col-sm-2 col-form-label" for="surname"><b>Surname</b></label>
-            <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.surname" readonly>
-            <br>
-            
-            
-            <label class="col-sm-2 col-form-label" for="email"><b>Email</b></label>
-            <input class="col-sm-2 col-form-control" type="email" v-model="activeUser.email" readonly>
-            <br>
-            
-            <label class="col-sm-2 col-form-label" for="password"><b>Password</b></label>
-            <input class="col-sm-2 col-form-control" type="password" v-model="activeUser.password" readonly>
-            <br>
-            
-            <label class="col-sm-2 col-form-label" for="password-repeat"><b>Repeat password</b></label>
-            <input class="col-sm-2 col-form-control" type="password" v-model="activeUser.password" name="password-repeat" id="password-repeat" readonly>
-            <br>
-            
-            <label class="col-sm-2 col-form-label" for="address"><b>Address</b></label>
-            <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.address" readonly>
-            <br>
-            
-            <label class="col-sm-2 col-form-label" for="city"><b>City</b></label>
-            <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.city" readonly>
-            <br>
 
-            <label class="col-sm-2 col-form-label" for="country"><b>Country</b></label>
-            <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.country" readonly>
-            <br>
-
-            <label class="col-sm-2 col-form-label" for="phoneNumber"><b>PhoneNumber</b></label>
-            <input class="col-sm-2 col-form-control" type="text" v-model="activeUser.phoneNumber" readonly>
-            <br>
-            
-        
-            <button type="button" class="button" v-on:click="editClick=true">Edit</button>
-        </div><hr>
-
-        <div style="margin-top: 4%; justify-content: center;">
-            <h4 v-if="sendCheck==false">Request for account deletion</h4>
-            <button type="button" class="button" v-if="requestDelete==false && sendCheck==false" v-on:click="requestDelete=true">Open request</button> 
-            <h4 v-if="sendCheck==true">Successfully sent request</h4>
-            
-            <div  v-if="requestDelete==true && sendCheck==false">
-                <textarea v-model="textAreaDelete" id="deleteArea" placeholder="Reason why you want to delete account" rows="4" cols="50"></textarea><br>
-                <button type="button" class="btn btn-primary" v-on:click="sendRequest()">Send request</button> 
-                <button type="button" class="btn btn-danger" v-on:click="requestDelete=false">Cancel</button> 
-            
+        <div class="row" style="margin-top:10%">
+            <div class= "col-md-4 container">
+                <h4 v-if="sendCheck==false">Request for account deletion</h4>
+                <button type="button" class="button" v-if="requestDelete==false && sendCheck==false" v-on:click="requestDelete=true">Open request</button> 
+                <h4 v-if="sendCheck==true">Successfully sent request</h4>
+                
+                <div  v-if="requestDelete==true && sendCheck==false">
+                    <textarea v-model="textAreaDelete" id="deleteArea" placeholder="Reason why you want to delete account" rows="4" cols="50"></textarea><br>
+                    <button type="button" class="btn btn-primary" v-on:click="sendRequest()">Send request</button> 
+                    <button type="button" class="btn btn-danger" v-on:click="requestDelete=false">Cancel</button> 
+                
+                </div>
+            </div>
+            <div class="col-md-2 razmak-card card">
+                <h2><span class="bi bi-star"></span>Broj bodova</h2>
+                <h3 style="text-align:center;">21</h3>
+            </div>
+            <div class="col-md-2 razmak-card card">
+                <h2><span class="bi bi-person"></span>Status</h2>
+                <h3 style="text-align:center;">Obican</h3>
+            </div>
+            <div class="col-md-2 razmak-card card">
+                <h2><span class="bi bi-people"></span>Vrsta korisnika</h2>
+                <h3 style="text-align:center;">Klijent</h3>
             </div>
         </div>
 
-    </div>	  
+    </div>
     	`
     	,
     methods: {
+        isEdit(){
+            this.editClick = true
+            document.getElementById("name").disabled = false 
+            document.getElementById("surname").disabled = false 
+            document.getElementById("email").disabled = false 
+            document.getElementById("password").disabled = false 
+            document.getElementById("confirmPassword").disabled = false
+            document.getElementById("address").disabled = false 
+            document.getElementById("city").disabled = false
+            document.getElementById("country").disabled = false 
+            document.getElementById("phoneNumber").disabled = false 
+
+        },
+        cancelEdit(){
+            this.activeUser = JSON.parse(localStorage.getItem('activeUser'))
+            this.editClick = false
+            document.getElementById("name").disabled = true 
+            document.getElementById("surname").disabled = true 
+            document.getElementById("email").disabled = true
+            document.getElementById("password").disabled = true 
+            document.getElementById("confirmPassword").disabled = true
+            document.getElementById("address").disabled = true 
+            document.getElementById("city").disabled = true
+            document.getElementById("country").disabled = true 
+            document.getElementById("phoneNumber").disabled = true 
+        },
         submitForm:function(){
             axios
             .put('/client/edit',this.activeUser)
             .then(response=>{
                 localStorage.setItem('activeUser',JSON.stringify(this.activeUser))
-                this.$router.push('/')
                 window.location.reload()
                 this.editClick = false
             })
@@ -163,6 +156,7 @@ Vue.component("ProfileClient", {
     },
     mounted(){
         this.activeUser = JSON.parse(localStorage.getItem('activeUser'))
+        this.helperUser = JSON.parse(localStorage.getItem('activeUser'))
         if(this.activeUser.role != 'client')
             this.$router.push('/')
 
--- BookingApplication/src/main/resources/public/APP/register.js
+++ BookingApplication/src/main/resources/public/APP/register.js
@@ -15,69 +15,69 @@ Vue.component("Register", {
         }
     },
     template:`  
-	<div>    
+	<div class="container">    
+        <div class="row">
+        <div class="col-lg-3 col-md-2"></div>
+        <div class="col-lg-6 col-md-8 login-box">
 		<form id="registrationForm" method ="POST" @submit.prevent = "submitForm">
 		  	<div class="container">
-			    <h1>Register</h1>
-			    <hr>
+                <div class="col-lg-12 login-key">
+                    <i class="bi bi-file-person" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">REGISTRACIJA</div><br>
 			
-			    <label class="col-sm-2 col-form-label" for="name"><b>Name</b></label>
-			    <input name="nameInput" pattern="[a-zA-Z]+" oninvalid="setCustomValidity('Please enter on letters only.')" class="col-sm-2 col-form-control" type="text" v-model="name" required>
+			    <label class="col-sm-4 col-form-label" for="name"><b>Name</b></label>
+			    <input name="nameInput" pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-4 col-form-control" type="text" v-model="name" required>
 				<br>
 				
-				<label class="col-sm-2 col-form-label" for="surname"><b>Surname</b></label>
-			    <input pattern="[a-zA-Z]+" oninvalid="setCustomValidity('Please enter on letters only.')" class="col-sm-2 col-form-control" type="text" v-model="surname" required>
+				<label class="col-sm-4 col-form-label" for="surname"><b>Surname</b></label>
+			    <input pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-4 col-form-control" type="text" v-model="surname" required>
 				<br>
-				
-				
-				<label class="col-sm-2 col-form-label" for="email"><b>Email</b></label>
-			    <input class="col-sm-2 col-form-control" type="email" v-model="email" required>
-				<br>
-
-
-                <label class="col-sm-2 col-form-label" for="password"><b>Password</b></label>
-			    <input id="password" class="col-sm-2 col-form-control" type="password" v-model="password" @change='check_pass()' required>
-				<br>
-				
-			    <label class="col-sm-2 col-form-label" for="password-repeat"><b>Repeat password</b></label>
-			    <input id="confirmPassword" class="col-sm-2 col-form-control" type="password" @change='check_pass()' required>
-			    <hr>
-
-				
-				<label class="col-sm-2 col-form-label" for="address"><b>Address</b></label>
-			    <input class="col-sm-2 col-form-control" type="text" v-model="address" required>
+							
+				<label class="col-sm-4 col-form-label" for="email"><b>Email</b></label>
+			    <input class="col-sm-4 col-form-control" type="email" v-model="email" required>
 				<br>
 
-                <label class="col-sm-2 col-form-label" for="city"><b>City</b></label>
-			    <input pattern="[a-zA-Z]+" oninvalid="setCustomValidity('Please enter on letters only.')" class="col-sm-2 col-form-control" type="text" v-model="city" required>
+                <label class="col-sm-4 col-form-label" for="password"><b>Password</b></label>
+			    <input id="password" class="col-sm-4 col-form-control" type="password" v-model="password" @change='check_pass()' required>
+				<br>
+				
+			    <label class="col-sm-4 col-form-label" for="password-repeat"><b>Repeat password</b></label>
+			    <input id="confirmPassword" class="col-sm-4 col-form-control" type="password" @change='check_pass()' required>
+			    <br>
+
+				<label class="col-sm-4 col-form-label" for="address"><b>Address</b></label>
+			    <input class="col-sm-4 col-form-control" type="text" v-model="address" required>
+				<br>
+
+                <label class="col-sm-4 col-form-label" for="city"><b>City</b></label>
+			    <input pattern="[a-zA-Z]+[a-zA-Z ]+" class="col-sm-4 col-form-control" type="text" v-model="city" required>
 				<br>
                 
-                <label class="col-sm-2 col-form-label" for="country"><b>Country</b></label>
-			    <input pattern="[a-zA-Z]+" oninvalid="setCustomValidity('Please enter on letters only.')" class="col-sm-2 col-form-control" type="text" v-model="country" required>
+                <label class="col-sm-4 col-form-label" for="country"><b>Country</b></label>
+			    <input pattern="[a-zA-Z]+[a-zA-Z ]+" title="Enter letters only." class="col-sm-4 col-form-control" type="text" v-model="country" required>
 				<br>
 
-                <label class="col-sm-2 col-form-label" for="phoneNumber"><b>PhoneNumber</b></label>
-			    <input pattern="[0-9]+" oninvalid="setCustomValidity('Please enter on numbers only.')" class="col-sm-2 col-form-control" type="text" v-model="phoneNumber" required>
+                <label class="col-sm-4 col-form-label" for="phoneNumber"><b>PhoneNumber</b></label>
+			    <input pattern="[0-9]+" title="Enter numbers only." class="col-sm-4 col-form-control" type="text" v-model="phoneNumber" required>
 				<br>
 
-
-                <label class="col-sm-2 col-form-label" for="role"><b>Role</b></label>
-				<select v-model="role" class="col-sm-2 col-form-select">
+                <label class="col-sm-4 col-form-label" for="role"><b>Role</b></label>
+				<select v-model="role" class="col-sm-4 col-form-select">
 					<option value="client">Client</option>
 				    <option value="cottage_owner">Cottage owner</option>
                     <option value="ship_owner">Ship owner</option>
                     <option value="fishing_instructor">Fishing instructor</option>
 				</select>
-				<br>
+				<br><br>
 				
-
-			
 			    <button id="submit" class="button" type="submit" disabled>Register</button>
 				<div class="container signin">
 		    		<p>Already have an account? <a href="#/login">Sign in</a>.</p>
 		  		</div>
 		  	</div>
 		</form>
+        </div></div></div>
 	</div>
     `       
         ,
